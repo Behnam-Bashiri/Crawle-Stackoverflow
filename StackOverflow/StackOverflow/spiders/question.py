99070d010fb63210478313fc53df0c4aa7716b6b
Behnam Bashiri <behnambashiri80@gmail.com>
2022-07-22 19:34:14 +0430

    first test is successfully ! 😎

---
+++ StackOverflow/StackOverflow/spiders/question.py
@@ -0,0 +1,35 @@
+from operator import le
+from turtle import title
+import scrapy
+
+
+class QuestionSpider(scrapy.Spider):
+    name = 'question'
+
+    def start_requests(self):
+        url_list = [
+
+        ]
+        for i in range(2,10):
+            url_list.append('https://stackoverflow.com/questions/tagged/python?page=%d' % i)
+
+        urls = []
+        urls.append('https://stackoverflow.com/questions')  
+        print("ok shooooooooooooood!")  
+        for url in url_list:
+            urls.append(url)
+            yield scrapy.Request(url=url, callback=self.parse)
+        
+
+    def parse(self, response):
+        title_question = response.xpath("//h3[@class='s-post-summary--content-title']/a/text()").extract()
+        body_question = response.xpath("//div[@class='s-post-summary--content-excerpt']/text()").extract()
+        author_question = response.xpath("//div[@class='s-user-card--link d-flex gs4']/a/text()").extract()
+
+        with open('question.txt', 'a') as f:
+            for i in range(len(title_question)):
+                f.write("******"+title_question[i]+"******"+'\n'\
+                    +body_question[i]+'\n'\
+                        +"@"+author_question[i]+'\n'\
+                            +"----------------------------------------------------------------"+'\n')
+            f.close()
